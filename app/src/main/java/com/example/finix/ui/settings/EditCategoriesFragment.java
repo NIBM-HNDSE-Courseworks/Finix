19440529ddf6102033a5982cd5d4549f9bd64669
GHOSTXMAN <niviruedu2006@gmail.com>
2025-10-27 22:15:50 +0530

    Files created for hirantha

---
+++ app/src/main/java/com/example/finix/ui/settings/EditCategoriesFragment.java
@@ -0,0 +1,38 @@
+package com.example.finix.ui.settings;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.finix.R;
+
+public class EditCategoriesFragment extends Fragment {
+
+    private EditCategoriesViewModel mViewModel;
+
+    public static EditCategoriesFragment newInstance() {
+        return new EditCategoriesFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_edit_categories, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(EditCategoriesViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/finix/ui/settings/EditCategoriesViewModel.java
@@ -0,0 +1,7 @@
+package com.example.finix.ui.settings;
+
+import androidx.lifecycle.ViewModel;
+
+public class EditCategoriesViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
