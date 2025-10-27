404177136d77d0d0d23fd3447f5d65d90d19a3cc
GHOSTXMAN <niviruedu2006@gmail.com>
2025-10-27 22:51:08 +0530

    Files name changed for Sachin

--- app/src/main/java/com/example/finix/ui/savings/SavingsGoalsFragment.java
+++ app/src/main/java/com/example/finix/ui/savings/SavingsGoalsFragment.java
@@ -61,6 +61,6 @@ public class SavingsGoalsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_savings, container, false);
+        return inflater.inflate(R.layout.fragment_savings_goals, container, false);
     }
 }
